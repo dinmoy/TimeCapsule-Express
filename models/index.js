56b953a29f8fcc2e4e791ed896420d4acc1caf40
fire-irror <firenoah.m@gmail.com>
2024-04-17 19:58:09 +0900

    feat: music 모델 추가

--- models/index.js
+++ models/index.js
@@ -1,7 +1,10 @@
 const sequelize=require('../config/database')
 
 const Letter=require('./letter')(sequelize)
+const Music=require('./music')(sequelize)
 
 module.exports={
-    Letter
+    Letter,
+    Music
 }
+
