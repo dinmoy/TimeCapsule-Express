1dde3575390feb10a0a0824cdf49571c8d783fd5
dinmoy <imhyojin614@gmail.com>
2024-04-17 09:16:14 +0900

    feat: 프로젝트 초기 설정

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+})
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to the config');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the config:', err);
+    });
+
+module.exports = sequelize;
